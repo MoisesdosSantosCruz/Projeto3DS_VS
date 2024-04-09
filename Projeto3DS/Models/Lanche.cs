bb4a8462de800b048633786314d726753607ea8f
MoisesdosSantosCruz <moises.cruz@etec.sp.gov.br>
2024-04-09 20:00:49 -0300

    Lançando ao GitHub

---
+++ Projeto3DS/Models/Lanche.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto3DS.Models
+{
+   
+    [Table ("Lanche")]
+    public class Lanche
+    {
+        public string descrisaocurta { get; set; }
+        public string descrisaodetalhada { get; set; }
+        
+        [Key] //
+        public int lancheid { get; set;}
+
+        public string nome { get; set; }
+        public decimal preco { get; set; }
+
+        public string imagemULR { get; set; }
+
+        public string lanchepreferido { get; set; }
+
+        public string imagemiminiauta { get; set; }
+
+        public int categoriaid {get; set;}
+
+        public string categoria {get; set; }
+
+
+
+
+
+
+
+    }
+}
